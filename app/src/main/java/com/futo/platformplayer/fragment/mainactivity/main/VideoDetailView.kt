be3e8f14d73230b28065d7d8bb2cfdafd7c0c937
Martín (Netux) Rodriguez <send.email@netux.site>
2024-02-13 22:38:01 -0300

    Update visibility of toggle alternative metadata button in minimized player in setVideoDetails()
    Not sure why there are two methods that do this, but they now do the same thing, so that's nice :^)

--- app/src/main/java/com/futo/platformplayer/fragment/mainactivity/main/VideoDetailView.kt
+++ app/src/main/java/com/futo/platformplayer/fragment/mainactivity/main/VideoDetailView.kt
@@ -1111,7 +1111,7 @@ class VideoDetailView : ConstraintLayout {
         if (video.alternativeName != null || video.thumbnails.hasAlternative()) {
             _buttonToggleAlternativeMetadata.visibility = VISIBLE;
             _buttonToggleAlternativeMetadataMinimized.visibility = VISIBLE;
-            setAlternativeMetadataButtonState(_buttonToggleAlternativeMetadata, _isAlternativeMetadataShown)
+            setAlternativeMetadataButtonState(_buttonToggleAlternativeMetadata, _isAlternativeMetadataShown);
         } else {
             _buttonToggleAlternativeMetadata.visibility = GONE;
             _buttonToggleAlternativeMetadataMinimized.visibility = GONE;
@@ -1293,9 +1293,11 @@ class VideoDetailView : ConstraintLayout {
 
         if (video.alternativeName != null || video.thumbnails.hasAlternative()) {
             _buttonToggleAlternativeMetadata.visibility = VISIBLE;
-            setAlternativeMetadataButtonState(_buttonToggleAlternativeMetadata, _isAlternativeMetadataShown)
+            _buttonToggleAlternativeMetadataMinimized.visibility = VISIBLE;
+            setAlternativeMetadataButtonState(_buttonToggleAlternativeMetadata, _isAlternativeMetadataShown);
         } else {
             _buttonToggleAlternativeMetadata.visibility = GONE;
+            _buttonToggleAlternativeMetadataMinimized.visibility = GONE;
         }
 
         _buttonSubscribe.setSubscribeChannel(video.author.url);
